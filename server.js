05fb128f984d2c99bee4330d0a856411f25aa070
vinay_varshney <vinay@ironsystems.com>
2021-08-21 15:41:42 +0530

    server.js

--- server.js
+++ server.js
@@ -42,6 +42,15 @@ app.use((req, res, next) => {
     next();
 });
 
+// sending client folder for deployement on base route
+if(process.env.NODE_ENV === 'production') {
+    // serve build  from client folder
+    app.use(express.static('client/build'));
+    app.get('*', (req,res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    });
+}
+
 // Routes middleware
 // app.use('/', baseRoute);
 app.use('/policy', policyRoute);
@@ -59,14 +68,7 @@ app.use((req, res, next) => {
     })
 })
 
-// sending client folder for deployement on base route
-if(process.env.NODE_ENV === 'production') {
-    // serve build  from client folder
-    app.use(express.static('client/build'));
-    app.get('*', (req,res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-    });
-}
+
 
 // Server ready
 app.listen(process.env.PORT || 3000);
