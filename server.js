7b81c9099f3f473d46cc2744ff422261764f8481
vinay_varshney <vinay@ironsystems.com>
2021-08-21 14:16:00 +0530

    heroku deployment

--- server.js
+++ server.js
@@ -59,6 +59,15 @@ app.use((req, res, next) => {
     })
 })
 
+// sending client folder for deployement on base route
+if(process.env.NODE_ENV === 'production') {
+    // serve build  from client folder
+    app.use(express.static('client/build'));
+    app.get('*', (req,res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    });
+}
+
 // Server ready
 app.listen(process.env.PORT || 3000);
 
